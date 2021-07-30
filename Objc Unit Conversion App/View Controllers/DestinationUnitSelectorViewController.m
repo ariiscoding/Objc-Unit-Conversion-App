ac7912935eb7b1467ce695fcd8ca35acfdecb8c0
Ari <ariqhe@gmail.com>
2021-07-30 16:32:27 -0700

    added a basic destination unit selector view controller

--- Objc Unit Conversion App/View Controllers/DestinationUnitSelectorViewController.m
+++ Objc Unit Conversion App/View Controllers/DestinationUnitSelectorViewController.m
@@ -13,6 +13,7 @@
 // MARK: - Properties
 
 NSArray *_destinationUnits;
+UISegmentedControl *_segmentedControl;
 
 // MARK: - Lifecycle
 
@@ -20,12 +21,35 @@ NSArray *_destinationUnits;
     [super viewDidLoad];
     
     [self injectAllAvailableUnits];
+    [self setUpSegmentedControl];
+    [self setUpConstraints];
+    
+    [self.view sizeToFit];
 }
 
 // MARK: - View Setup
-
+- (void)setUpSegmentedControl {
+    _segmentedControl = [[UISegmentedControl alloc] initWithItems: [self getAvailableUnitsStringArray]];
+    
+    [self.view addSubview:_segmentedControl];
+}
 
 // MARK: - Constraints
+
+- (void)setUpConstraints {
+    _segmentedControl.translatesAutoresizingMaskIntoConstraints = FALSE;
+    
+    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:_segmentedControl attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:0];
+    
+    NSLayoutConstraint *bottom = [NSLayoutConstraint constraintWithItem:_segmentedControl attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0];
+    
+    NSLayoutConstraint *leading = [NSLayoutConstraint constraintWithItem:_segmentedControl attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0];
+    
+    NSLayoutConstraint *trailing = [NSLayoutConstraint constraintWithItem:_segmentedControl attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0];
+    
+    NSArray *constraints = [NSArray arrayWithObjects:top, bottom, leading, trailing, nil];
+    [self.view addConstraints:constraints];
+}
 
 // MARK: - Helper Methods
 
@@ -36,6 +60,18 @@ NSArray *_destinationUnits;
     OutputWeightUnit *mile = OutputWeightUnit.mile;
     
     _destinationUnits = [NSArray arrayWithObjects:cm, meter, feet, mile, nil];
+}
+
+- (NSArray*)getAvailableUnitsStringArray {
+    NSMutableArray *array = [NSMutableArray arrayWithCapacity:_destinationUnits.count];
+    
+    for (int i = 0; i < _destinationUnits.count; i++) {
+        OutputWeightUnit *unit = [_destinationUnits objectAtIndex: i];
+        
+        [array replaceObjectAtIndex:i withObject:unit];
+    }
+    
+    return [[NSArray alloc] initWithArray:array];
 }
 
 @end
